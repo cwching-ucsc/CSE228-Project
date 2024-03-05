52a66187904ebddf072a022e8faf6cb974c3f583
Tongze Wang <wtongze@gmail.com>
2024-03-04 18:46:51 -0800

    feat: complete IPv4SubnetUtil

--- src/main/scala/network/SubnetUtil.scala
+++ src/main/scala/network/SubnetUtil.scala
@@ -1,8 +1,45 @@
 package network
 
-object SubnetUtil {
-  def getStartAddr(ip: IPv4Addr, mask: IPv4Addr): IPv4Addr = {
-    assert(ip.width == mask.width)
-    new IPv4Addr(ip.addr & mask.addr)
-  }
+trait SubnetUtil[T <: NetworkAddr] {
+  /**
+   * Get the start address of the subnet
+   * @param ip `NetworkAddr` IP
+   * @param mask `NetworkAddr` Network mask
+   * @return `NetworkAddr`
+   */
+  def getStartAddr(ip: T, mask: T): T
+
+  /**
+   * Get the end address of the subnet
+   * @param ip `NetworkAddr` IP
+   * @param mask `NetworkAddr` Network mask
+   * @return `NetworkAddr`
+   */
+  def getEndAddr(ip: T, mask: T): T
+
+  /**
+   * Get all addresses of the subnet in a `Vector`
+   * @param ip `NetworkAddr` IP
+   * @param mask `NetworkAddr` Network mask
+   * @return `Vector[NetworkAddr]`
+   */
+  def getRangeVector(ip: T, mask: T): Vector[T]
+
+  /**
+   * Check if given target IP is in the subnet
+   * @param target `NetworkAddr` Target
+   * @param ip `NetworkAddr` IP
+   * @param mask `NetworkAddr` Network mask
+   * @return `Boolean`
+   */
+  def isInSubnet(target: T, ip: T, mask: T): Boolean
+
+  /**
+   * Check if given target IP is a special broadcast address
+   * @param target `NetworkAddr` Target
+   * @param ip `NetworkAddr` IP
+   * @param mask `NetworkAddr` Network mask
+   * @return `Boolean`
+   */
+  def isBroadCastAddr(target: T, ip: T, mask: T): Boolean
 }
