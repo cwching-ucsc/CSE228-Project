e9ced7a8c6c90a930662affd47c767d891194362
Tongze Wang <wtongze@gmail.com>
2024-03-10 19:57:32 -0700

    feat: update wildcard tests

--- src/test/scala/cam/TCAMTester.scala
+++ src/test/scala/cam/TCAMTester.scala
@@ -6,8 +6,8 @@ import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class TCAMTester extends AnyFlatSpec with ChiselScalatestTester {
-  // 3 * 32 bit entries
-  val p = CAMParams(3, 32)
+  // 3 * 4 bit entries
+  val p = CAMParams(3, 4)
 
   def outputCheck(dut: TCAM): Unit = {
     dut.io.full.expect(false.B)
@@ -51,7 +51,8 @@ class TCAMTester extends AnyFlatSpec with ChiselScalatestTester {
     test(new TCAM(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       dut.io.in.valid.poke(true.B)
       dut.io.in.bits.cmds.poke(buildWriteCmd())
-      dut.io.in.bits.content.poke(1.U)
+      dut.io.in.bits.content.poke(0x1.U) // 0001
+      dut.io.in.bits.mask.poke(0x1.U) // 0001
       outputCheck(dut)
       dut.io.out.bits.expect(0.U)
     }
@@ -61,14 +62,16 @@ class TCAMTester extends AnyFlatSpec with ChiselScalatestTester {
     test(new TCAM(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       dut.io.in.valid.poke(true.B)
       dut.io.in.bits.cmds.poke(buildWriteCmd())
-      dut.io.in.bits.content.poke(10.U)
+      dut.io.in.bits.content.poke(0xA.U) // 1010
+      dut.io.in.bits.mask.poke(0x1.U) // 0001
 
       outputCheck(dut)
       dut.io.out.bits.expect(0.U)
 
       dut.clock.step()
 
-      dut.io.in.bits.content.poke(20.U)
+      dut.io.in.bits.content.poke(0xB.U) // 1011
+      dut.io.in.bits.mask.poke(0x2) // 0010
 
       outputCheck(dut)
       dut.io.out.bits.expect(1.U)
@@ -80,25 +83,29 @@ class TCAMTester extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.in.valid.poke(true.B)
       dut.io.in.bits.cmds.poke(buildWriteCmd())
 
-      dut.io.in.bits.content.poke(10.U)
+      dut.io.in.bits.content.poke(0xA.U) // 1010
+      dut.io.in.bits.mask.poke(0x1.U) // 0001
       outputCheck(dut)
       dut.io.out.bits.expect(0.U)
 
       dut.clock.step()
 
-      dut.io.in.bits.content.poke(20.U)
+      dut.io.in.bits.content.poke(0xB.U) // 1011
+      dut.io.in.bits.mask.poke(0x2) // 0010
       outputCheck(dut)
       dut.io.out.bits.expect(1.U)
 
       dut.clock.step()
 
-      dut.io.in.bits.content.poke(30.U)
+      dut.io.in.bits.content.poke(0xC.U) // 1100
+      dut.io.in.bits.mask.poke(0x3) // 0011
       outputCheck(dut)
       dut.io.out.bits.expect(2.U)
 
       dut.clock.step()
 
-      dut.io.in.bits.content.poke(40.U)
+      dut.io.in.bits.content.poke(0xD.U) // 1101
+      dut.io.in.bits.mask.poke(0x4) // 0100
       dut.io.full.expect(true.B)
       dut.io.out.valid.expect(false.B)
     }
@@ -108,32 +115,36 @@ class TCAMTester extends AnyFlatSpec with ChiselScalatestTester {
     test(new TCAM(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       dut.io.in.valid.poke(true.B)
       dut.io.in.bits.cmds.poke(buildWriteCmd())
-      dut.io.in.bits.content.poke(10.U)
+      dut.io.in.bits.content.poke(0xA.U) // 1010
+      dut.io.in.bits.mask.poke(0x0.U) // 0000
       outputCheck(dut)
       dut.io.out.bits.expect(0.U)
 
       dut.clock.step()
 
-      dut.io.in.bits.content.poke(20.U)
+      dut.io.in.bits.content.poke(0xB.U) // 1011
+      dut.io.in.bits.mask.poke(0x0.U) // 0000
       outputCheck(dut)
       dut.io.out.bits.expect(1.U)
 
       dut.clock.step()
 
-      dut.io.in.bits.content.poke(30.U)
+      dut.io.in.bits.content.poke(0xC.U) // 1100
+      dut.io.in.bits.mask.poke(0x0.U) // 0000
       outputCheck(dut)
       dut.io.out.bits.expect(2.U)
 
       dut.clock.step()
 
-      dut.io.in.bits.content.poke(40.U)
+      dut.io.in.bits.content.poke(0xD.U) // 1101
+      dut.io.in.bits.mask.poke(0x0.U) // 0000
       dut.io.full.expect(true.B)
       dut.io.out.valid.expect(false.B)
 
       dut.clock.step()
 
       dut.io.in.bits.cmds.poke(buildDeleteCmd())
-      dut.io.in.bits.content.poke(20.U)
+      dut.io.in.bits.content.poke(0xB.U) // 1011
       dut.io.full.expect(true.B)
       dut.io.out.valid.expect(true.B)
       dut.io.out.bits.expect(1.U)
@@ -141,7 +152,8 @@ class TCAMTester extends AnyFlatSpec with ChiselScalatestTester {
       dut.clock.step()
 
       dut.io.in.bits.cmds.poke(buildWriteCmd())
-      dut.io.in.bits.content.poke(50.U)
+      dut.io.in.bits.content.poke(0xE.U) // 1110
+      dut.io.in.bits.mask.poke(0x0.U) // 0000
       outputCheck(dut)
       dut.io.out.bits.expect(1.U)
     }
@@ -151,14 +163,33 @@ class TCAMTester extends AnyFlatSpec with ChiselScalatestTester {
     test(new TCAM(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       dut.io.in.valid.poke(true.B)
       dut.io.in.bits.cmds.poke(buildWriteCmd())
-      dut.io.in.bits.content.poke(1.U)
+      dut.io.in.bits.content.poke(0x5.U) // 0101
+      dut.io.in.bits.mask.poke(0x3.U) // 0011
       outputCheck(dut)
       dut.io.out.bits.expect(0.U)
 
       dut.clock.step()
 
       dut.io.in.bits.cmds.poke(buildReadCmd())
-      dut.io.in.bits.content.poke(1.U)
+      dut.io.in.bits.content.poke(0x5.U) // 0101
+      dut.io.out.valid.expect(true.B)
+      dut.io.out.bits.expect(0.U)
+    }
+  }
+
+  it should "able to write and read 1 entry using wildcard" in {
+    test(new TCAM(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.io.in.valid.poke(true.B)
+      dut.io.in.bits.cmds.poke(buildWriteCmd())
+      dut.io.in.bits.content.poke(0x5.U) // 0101
+      dut.io.in.bits.mask.poke(0x3.U) // 0011
+      outputCheck(dut)
+      dut.io.out.bits.expect(0.U)
+
+      dut.clock.step()
+
+      dut.io.in.bits.cmds.poke(buildReadCmd())
+      dut.io.in.bits.content.poke(0x4.U) // 0100
       dut.io.out.valid.expect(true.B)
       dut.io.out.bits.expect(0.U)
     }
@@ -168,14 +199,32 @@ class TCAMTester extends AnyFlatSpec with ChiselScalatestTester {
     test(new TCAM(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       dut.io.in.valid.poke(true.B)
       dut.io.in.bits.cmds.poke(buildWriteCmd())
-      dut.io.in.bits.content.poke(1.U)
+      dut.io.in.bits.content.poke(0x1.U) // 0001
+      dut.io.in.bits.mask.poke(0x0.U) // 0000
       outputCheck(dut)
       dut.io.out.bits.expect(0.U)
 
       dut.clock.step()
 
       dut.io.in.bits.cmds.poke(buildReadCmd())
-      dut.io.in.bits.content.poke(2.U)
+      dut.io.in.bits.content.poke(0x2.U) // 0010
+      dut.io.out.valid.expect(false.B)
+    }
+  }
+
+  it should "report not valid when entry not found in read using wildcard" in {
+    test(new TCAM(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.io.in.valid.poke(true.B)
+      dut.io.in.bits.cmds.poke(buildWriteCmd())
+      dut.io.in.bits.content.poke(0x1.U) // 0001
+      dut.io.in.bits.mask.poke(0x1.U) // 0001
+      outputCheck(dut)
+      dut.io.out.bits.expect(0.U)
+
+      dut.clock.step()
+
+      dut.io.in.bits.cmds.poke(buildReadCmd())
+      dut.io.in.bits.content.poke(0x2.U) // 0010
       dut.io.out.valid.expect(false.B)
     }
   }
@@ -184,14 +233,33 @@ class TCAMTester extends AnyFlatSpec with ChiselScalatestTester {
     test(new TCAM(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       dut.io.in.valid.poke(true.B)
       dut.io.in.bits.cmds.poke(buildWriteCmd())
-      dut.io.in.bits.content.poke(1.U)
+      dut.io.in.bits.content.poke(0x1.U) // 0001
+      dut.io.in.bits.mask.poke(0x1) // 0001
       outputCheck(dut)
       dut.io.out.bits.expect(0.U)
 
       dut.clock.step()
 
       dut.io.in.bits.cmds.poke(buildDeleteCmd())
-      dut.io.in.bits.content.poke(1.U)
+      dut.io.in.bits.content.poke(0x1.U) // 0001
+      outputCheck(dut)
+      dut.io.out.bits.expect(0.U)
+    }
+  }
+
+  it should "able to delete 1 entry using wildcard" in {
+    test(new TCAM(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.io.in.valid.poke(true.B)
+      dut.io.in.bits.cmds.poke(buildWriteCmd())
+      dut.io.in.bits.content.poke(0x1.U) // 0001
+      dut.io.in.bits.mask.poke(0x1) // 0001
+      outputCheck(dut)
+      dut.io.out.bits.expect(0.U)
+
+      dut.clock.step()
+
+      dut.io.in.bits.cmds.poke(buildDeleteCmd())
+      dut.io.in.bits.content.poke(0x0.U) // 0000
       outputCheck(dut)
       dut.io.out.bits.expect(0.U)
     }
@@ -210,28 +278,30 @@ class TCAMTester extends AnyFlatSpec with ChiselScalatestTester {
     test(new TCAM(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       dut.io.in.valid.poke(true.B)
       dut.io.in.bits.cmds.poke(buildWriteCmd())
-      dut.io.in.bits.content.poke(1.U)
+      dut.io.in.bits.content.poke(0x1.U) // 0001
+      dut.io.in.bits.mask.poke(0x0.U) // 0000
       outputCheck(dut)
       dut.io.out.bits.expect(0.U)
 
       dut.clock.step()
 
       dut.io.in.bits.cmds.poke(buildWriteCmd())
-      dut.io.in.bits.content.poke(2.U)
+      dut.io.in.bits.content.poke(0x2.U) // 0010
+      dut.io.in.bits.mask.poke(0x0.U) // 0000
       outputCheck(dut)
       dut.io.out.bits.expect(1.U)
 
       dut.clock.step()
 
       dut.io.in.bits.cmds.poke(buildReadCmd())
-      dut.io.in.bits.content.poke(1.U)
+      dut.io.in.bits.content.poke(0x1.U) // 0001
       dut.io.out.valid.expect(true.B)
       dut.io.out.bits.expect(0.U)
 
       dut.clock.step()
 
       dut.io.in.bits.cmds.poke(buildReadCmd())
-      dut.io.in.bits.content.poke(2.U)
+      dut.io.in.bits.content.poke(0x2.U) // 0010
       dut.io.out.valid.expect(true.B)
       dut.io.out.bits.expect(1.U)
 
@@ -244,13 +314,67 @@ class TCAMTester extends AnyFlatSpec with ChiselScalatestTester {
       dut.clock.step()
 
       dut.io.in.bits.cmds.poke(buildReadCmd())
-      dut.io.in.bits.content.poke(1.U)
+      dut.io.in.bits.content.poke(0x1.U) // 0001
       dut.io.out.valid.expect(false.B)
 
       dut.clock.step()
 
       dut.io.in.bits.cmds.poke(buildWriteCmd())
-      dut.io.in.bits.content.poke(1.U)
+      dut.io.in.bits.content.poke(0x1.U) // 0001
+      dut.io.in.bits.mask.poke(0x0) // 0000
+      outputCheck(dut)
+      dut.io.out.bits.expect(0.U)
+    }
+  }
+
+  it should "able to reset all entries using wildcard" in {
+    test(new TCAM(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.io.in.valid.poke(true.B)
+      dut.io.in.bits.cmds.poke(buildWriteCmd())
+      dut.io.in.bits.content.poke(0x1.U) // 0001
+      dut.io.in.bits.mask.poke(0x1.U) // 0001
+      outputCheck(dut)
+      dut.io.out.bits.expect(0.U)
+
+      dut.clock.step()
+
+      dut.io.in.bits.cmds.poke(buildWriteCmd())
+      dut.io.in.bits.content.poke(0x2.U) // 0010
+      dut.io.in.bits.mask.poke(0x1.U) // 0001
+      outputCheck(dut)
+      dut.io.out.bits.expect(1.U)
+
+      dut.clock.step()
+
+      dut.io.in.bits.cmds.poke(buildReadCmd())
+      dut.io.in.bits.content.poke(0x0.U) // 0000
+      dut.io.out.valid.expect(true.B)
+      dut.io.out.bits.expect(0.U)
+
+      dut.clock.step()
+
+      dut.io.in.bits.cmds.poke(buildReadCmd())
+      dut.io.in.bits.content.poke(0x3.U) // 0011
+      dut.io.out.valid.expect(true.B)
+      dut.io.out.bits.expect(1.U)
+
+      dut.clock.step()
+
+      dut.io.in.bits.cmds.poke(buildResetCmd())
+      dut.io.out.valid.expect(true.B)
+      dut.io.out.bits.expect((p.entries - 1).U)
+
+      dut.clock.step()
+
+      dut.io.in.bits.cmds.poke(buildReadCmd())
+      dut.io.in.bits.content.poke(0x0.U) // 0000
+      dut.io.out.valid.expect(false.B)
+
+      dut.clock.step()
+
+      dut.io.in.bits.cmds.poke(buildWriteCmd())
+      dut.io.in.bits.content.poke(0x1.U) // 0001
+      dut.io.in.bits.mask.poke(0x1) // 0001
       outputCheck(dut)
       dut.io.out.bits.expect(0.U)
     }
